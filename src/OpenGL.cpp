69530cd89b14cbc4481502a2382c38558e258f23
Victor <Vitoludde@gmail.com>
2023-04-17 19:33:50 +0200

    Clean up

--- src/OpenGL.cpp
+++ src/OpenGL.cpp
@@ -136,7 +136,7 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	data = stbi_load("images/trollface.png", &width, &height, &nrChannels, 4);
+	data = stbi_load("images/container.png", &width, &height, &nrChannels, 4);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
